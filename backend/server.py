0a21b78805bc88d4fd7c480cb61b34b87501fa0f
Annawint01 <metbrandscout@outlook.com>
2026-04-16 21:36:02 +0100

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -200,7 +200,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
+    allow_origins=["https://metbrandscout.netlify.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
